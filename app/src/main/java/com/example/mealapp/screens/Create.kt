4304bc0d6791bc3e0df8dcbe9baf0e8e971b6096
Aishwarya Velu <av382@cornell.edu>
2022-07-27 01:11:35 -0400

    Added images to drop down menu

--- app/src/main/java/com/example/mealapp/screens/Create.kt
+++ app/src/main/java/com/example/mealapp/screens/Create.kt
@@ -1,4 +1,5 @@
 package com.example.mealapp.screens
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,13 +9,16 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,62 +47,160 @@ fun CreateScreen() {
 private fun Ingredients(count: List<String> = List(3) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = count) {
-            IngredientDropDownMenu()
+            TopAppBarDropdownMenu()
+        }
+    }
+}
+
+@Composable
+fun NestedMenu(onValueChange: () -> Unit, expandedNested:MutableState<Boolean>,
+               selectedText:MutableState<String>, textfieldSize:MutableState<Size>,
+               listData: List<ProductModel>, label: String) {
+
+    DropdownMenu(
+        expanded = expandedNested.value,
+        onDismissRequest = { expandedNested.value = false },
+        modifier = Modifier
+            .width(with(LocalDensity.current) { textfieldSize.value.width.toDp() })
+    ) {
+        listData.forEach { label ->
+            DropdownMenuItem(onClick = {
+                selectedText.value = label.title
+                expandedNested.value = false
+                onValueChange.invoke()
+            }) {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Text(text = label.title)
+                    Spacer(modifier = Modifier.weight(1f))
+                    Image( painter = painterResource(id = label.image),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.requiredSize(50.dp))
+                }
+            }
         }
     }
 }
 @Composable
-fun IngredientDropDownMenu() {
-    var expanded by remember { mutableStateOf(false) }
+fun TopAppBarDropdownMenu() {
+    var expandedMain = remember { mutableStateOf(false) }
+    var expandedNested = remember { mutableStateOf(false) }
+    var expandedNestedLabel = remember { mutableStateOf("") }
     val suggestions = listOf("Meats", "Dairy", "Fruits", "Vegetables")
     val meatsList = Data.meatData["Meats"]
     val dairyList = Data.dairyData["Dairy"]
     val vegetableList = Data.vegetableData["Vegetables"]
     val fruitList = Data.fruitData["Fruits"]
-    var selectedText by remember { mutableStateOf("") }
-    var textfieldSize by remember { mutableStateOf(Size.Zero)}
+    var selectedText = remember { mutableStateOf("") }
+    var textfieldSize = remember { mutableStateOf(Size.Zero) }
 
-    val icon = if (expanded)
+    val iconMain = if (expandedMain.value)
         Icons.Filled.KeyboardArrowUp
     else
         Icons.Filled.KeyboardArrowDown
 
+    val iconNested = if (expandedNested.value)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
 
     Column(Modifier.padding(20.dp)) {
         OutlinedTextField(
-            value = selectedText,
-            onValueChange = { selectedText = it },
+            value = selectedText.value,
+            onValueChange = { selectedText.value = it },
             modifier = Modifier
                 .fillMaxWidth()
                 .onGloballyPositioned { coordinates ->
                     //This value is used to assign to the DropDown the same width
-                    textfieldSize = coordinates.size.toSize()
+                    textfieldSize.value = coordinates.size.toSize()
                 },
-            label = {Text("Label")},
+            label = { Text("Label") },
             trailingIcon = {
-                Icon(icon,"contentDescription",
-                    Modifier.clickable { expanded = !expanded })
+                Icon(iconMain, "contentDescription",
+                    Modifier.clickable {
+                        // Expand the main menu on icon click
+                        // and hide the nested menu.
+                        expandedMain.value = true
+                        expandedNested.value = false
+                    })
             }
         )
         DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
+            expanded = expandedMain.value,
+            onDismissRequest = { expandedMain.value = false },
             modifier = Modifier
-                .width(with(LocalDensity.current){textfieldSize.width.toDp()})
+                .width(with(LocalDensity.current){textfieldSize.value.width.toDp()})
         ) {
             suggestions.forEach { label ->
                 DropdownMenuItem(onClick = {
-                    selectedText = label
-                    expanded = false
-                }) {
-                    Text(text = label)
+                    expandedMain.value = false
+                    expandedNested.value = true
+                    expandedNestedLabel.value = label
+                },
+                ){
+                    Row(horizontalArrangement = Arrangement.Start) {
+                        Text(text = label)
+                        Icon(imageVector = iconNested,
+                            //modifier = Modifier.weight(6f, true),
+                            contentDescription = null)
+                    }
                 }
             }
         }
+        when (expandedNestedLabel.value) {
+            "Meats" -> meatsList?.let {
+                NestedMenu(
+                    onValueChange = {
+                        expandedMain.value = false
+                    },
+                    expandedNested = expandedNested,
+                    selectedText = selectedText,
+                    textfieldSize = textfieldSize,
+                    listData = it,
+                    label = expandedNestedLabel.value
+                )
+            }
+            "Dairy" -> dairyList?.let {
+                NestedMenu(
+                    onValueChange = {
+                        expandedMain.value = false
+                    },
+                    expandedNested = expandedNested,
+                    selectedText = selectedText,
+                    textfieldSize = textfieldSize,
+                    listData = it,
+                    label = expandedNestedLabel.value
+                )
+            }
+            "Vegetables" -> vegetableList?.let {
+                NestedMenu(
+                    onValueChange = {
+                        expandedMain.value = false
+                    },
+                    expandedNested = expandedNested,
+                    selectedText = selectedText,
+                    textfieldSize = textfieldSize,
+                    listData = it,
+                    label = expandedNestedLabel.value
+                )
+            }
+            "Fruits"-> fruitList?.let {
+                NestedMenu(
+                    onValueChange = {
+                        expandedMain.value = false
+                    },
+                    expandedNested = expandedNested,
+                    selectedText = selectedText,
+                    textfieldSize = textfieldSize,
+                    listData = it,
+                    label = expandedNestedLabel.value
+                )
+            }
+            else -> {}
+
+        }
     }
-
 }
-
 @Composable
 @Preview
 fun CreateScreenPreview() {
--- app/src/main/java/com/example/mealapp/screens/Home.kt
+++ app/src/main/java/com/example/mealapp/screens/Home.kt
@@ -1,5 +1,6 @@
 package com.example.mealapp.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -7,14 +8,17 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,157 +33,12 @@ fun HomeScreen() {
             .background(Color.Magenta),
         contentAlignment = Alignment.Center
     ) {
-//        Text(
-//            text = "HOME",
-//            fontSize = MaterialTheme.typography.h3.fontSize,
-//            fontWeight = FontWeight.Bold,
-//            color = Color.White
-//        )
-        TopAppBarDropdownMenu()
-    }
-}
-
-@Composable
-fun NestedMenu(expandedNested:MutableState<Boolean>, selectedText:MutableState<String>,
-               textfieldSize:MutableState<Size>, listData: List<ProductModel>, label: String) {
-
-    val icon = if (expandedNested.value)
-        Icons.Filled.KeyboardArrowUp
-    else
-        Icons.Filled.KeyboardArrowDown
-
-    Column(Modifier.padding(20.dp)) {
-        OutlinedTextField(
-            value = label,
-            onValueChange = { },
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    //This value is used to assign to the DropDown the same width
-                    textfieldSize.value = coordinates.size.toSize()
-                },
-            label = { Text("Label") },
-            trailingIcon = {
-                Icon(icon, "contentDescription",
-                    Modifier.clickable { expandedNested.value = !expandedNested.value })
-            }
+        Text(
+            text = "HOME",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
-        DropdownMenu(
-            expanded = expandedNested.value,
-            onDismissRequest = { expandedNested.value = false },
-            modifier = Modifier
-                .width(with(LocalDensity.current) { textfieldSize.value.width.toDp() })
-        ) {
-            listData.forEach { label ->
-                DropdownMenuItem(onClick = {
-                    selectedText.value = label.title
-                    expandedNested.value = false
-                }) {
-                    Text(text = label.title)
-                }
-            }
-        }
-    }
-}
-@Composable
-fun TopAppBarDropdownMenu() {
-    var expandedMain = remember { mutableStateOf(false) }
-    var expandedNestedMeats = remember { mutableStateOf(false) }
-    var expandedNestedDairy = remember { mutableStateOf(false) }
-    var expandedNestedFruits = remember { mutableStateOf(false) }
-    var expandedNestedVegetables = remember { mutableStateOf(false) }
-    val suggestions = listOf("Meats", "Dairy", "Fruits", "Vegetables")
-    val meatsList = Data.meatData["Meats"]
-    val dairyList = Data.dairyData["Dairy"]
-    val vegetableList = Data.vegetableData["Vegetables"]
-    val fruitList = Data.fruitData["Fruits"]
-    var selectedText = remember { mutableStateOf("") }
-    var textfieldSize = remember { mutableStateOf(Size.Zero) }
-
-    val iconMain = if (expandedMain.value)
-        Icons.Filled.KeyboardArrowUp
-    else
-        Icons.Filled.KeyboardArrowDown
-
-    Column(Modifier.padding(20.dp)) {
-        OutlinedTextField(
-            value = selectedText.value,
-            onValueChange = { selectedText.value = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    //This value is used to assign to the DropDown the same width
-                    textfieldSize.value = coordinates.size.toSize()
-                },
-            label = { Text("Label") },
-            trailingIcon = {
-                Icon(iconMain, "contentDescription",
-                    Modifier.clickable {
-                        // Expand the main menu on icon click
-                        // and hide the nested menu.
-                        expandedMain.value = true
-                        expandedNestedMeats.value = false
-                        expandedNestedDairy.value = false
-                        expandedNestedFruits.value = false
-                        expandedNestedVegetables.value = false
-                    })
-            }
-        )
-        DropdownMenu(
-            expanded = expandedMain.value,
-            onDismissRequest = { expandedMain.value = false },
-            modifier = Modifier
-                .width(with(LocalDensity.current){textfieldSize.value.width.toDp()})
-        ) {
-            suggestions.forEach { label ->
-                DropdownMenuItem(onClick = {
-                    expandedMain.value = false
-                    //expandedNested.value = true
-                },
-                content = {
-                    when (label) {
-                        "Meats" -> meatsList?.let {
-                            NestedMenu(
-                                expandedNested = expandedNestedMeats,
-                                selectedText = selectedText,
-                                textfieldSize = textfieldSize,
-                                listData = it,
-                                label = label
-                            )
-                        }
-                        "Dairy" -> dairyList?.let {
-                            NestedMenu(
-                                expandedNested = expandedNestedDairy,
-                                selectedText = selectedText,
-                                textfieldSize = textfieldSize,
-                                listData = it,
-                                label = label
-                            )
-                        }
-                        "Vegetables" -> vegetableList?.let {
-                            NestedMenu(
-                                expandedNested = expandedNestedVegetables,
-                                selectedText = selectedText,
-                                textfieldSize = textfieldSize,
-                                listData = it,
-                                label = label
-                            )
-                        }
-                        else -> fruitList?.let {
-                            NestedMenu(
-                                expandedNested = expandedNestedMeats,
-                                selectedText = selectedText,
-                                textfieldSize = textfieldSize,
-                                listData = it,
-                                label = label
-                            )
-                        }
-
-                    }
-                })
-                Text(text = label)
-            }
-        }
     }
 }
 
